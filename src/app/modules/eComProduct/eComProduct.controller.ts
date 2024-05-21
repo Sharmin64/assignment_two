e3dd088119f527f12d72db6dde42e2aeddec2f69
Sharmin64 <lizaeatsbanana@gmail.com>
2024-05-21 22:01:25 +0600

    controller function done

--- src/app/modules/eComProduct/eComProduct.controller.ts
+++ src/app/modules/eComProduct/eComProduct.controller.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from 'express';
+import { ProductService } from './eComProduct.service';
 
-const cteateProduct = (req: Request, res: Response) => {
-  const product = req.body;
+const cteateProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
 
-  // will call service func to send this data
+    // will call service func to send this data
+    const result = await ProductService.createProductIntoDB(product);
 
-  // will send response from here
+    // will send response from here
+
+    res.status(200).json({
+      success: true,
+      message: 'Product Created Successfully!',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const ProductController = {
+  cteateProduct,
 };
