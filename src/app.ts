e6a2b1990d4bf5fdd005369a9820c73a14632687
Sharmin64 <lizaeatsbanana@gmail.com>
2024-05-21 12:30:45 +0600

    eslint prettier set up

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+// const port = 3000;
+
+// persers
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Assignment!");
+});
+
+export default app;
